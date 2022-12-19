5d48edbfd0dd326ab580ad467a1c0a2b61aefdb7
CrossStackQ <crossstack@gmail.com>
2022-12-19 21:22:29 +0530

    ko

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ async function main() {
 
 main()
   .then(() => {
-    console.log("Finished successfully   kio")
+    console.log("Finished successfully   k")
     process.exit(0)
   })
   .catch((error) => {
